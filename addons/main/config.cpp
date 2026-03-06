cf09a9c4244d85c375e79ec589f9bbc239e41f2f
StepanKo101 <stepankosvincev@gmail.com>
2026-03-06 16:54:28 +0500

    First commit

---
+++ addons/main/config.cpp
@@ -0,0 +1,15 @@
+class CfgPatches
+{
+    class fte_Main
+    {
+        name = "Fortify Tool Enabled - Main";
+        author = "StepanKo101";
+        url = "";
+        
+        requiredVersion = 0.50;
+        requiredAddons[] = {};
+        
+        units[] = {};
+        weapons[] = {};
+    };
+};
